15c771e43ded27279422832a9444e448a05c640d
net11FellaJ <julian.fella@neteleven.de>
2024-08-06 18:43:57 +0200

    add scroll text to home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,10 @@
 import heroAnimation from '../animations/hero';
+import scrollText from '../animations/shared/scrollText';
 
 function init() {
   console.log('home init');
   heroAnimation.init();
+  scrollText.init();
 }
 
 function cleanup() {

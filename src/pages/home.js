fc91ac526607f4ac1ca4345b1f8c2f4fc1bff27e
net11FellaJ <julian.fella@neteleven.de>
2024-08-09 14:05:10 +0200

    home.js update of imports

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,17 @@
 import shared from '../animations/shared.js';
-import scrollLogo from '../animations/scrollLogo.js';
+import scrollLogo from '../animations/home/scrollLogo.js';
+import scrollService from '../animations/home/scrollService.js';
 
 function init() {
   console.log('home init');
   scrollLogo.init();
+  scrollService.init();
   shared.init();
 }
 
 function cleanup() {
   scrollLogo.cleanup();
+  scrollService.cleanup();
   shared.cleanup();
 }
 

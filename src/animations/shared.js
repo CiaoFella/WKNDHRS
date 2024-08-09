053337ff0b320a06b978012bf4b333469190bf86
net11FellaJ <julian.fella@neteleven.de>
2024-08-09 18:55:20 +0200

    move hero to shared

--- src/animations/shared.js
+++ src/animations/shared.js
@@ -1,12 +1,15 @@
+import hero from './shared/hero.js';
 import scrollParallax from './shared/scrollParallax.js';
 import scrollText from './shared/scrollText.js';
 
 function init() {
+  hero.init();
   scrollParallax.init();
   scrollText.init();
 }
 
 function cleanup() {
+  hero.cleanup();
   scrollParallax.cleanup();
   scrollText.cleanup();
 }

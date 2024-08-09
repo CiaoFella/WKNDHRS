479f0ec3774db2542a4017dc98cb02f9f3e52513
net11FellaJ <julian.fella@neteleven.de>
2024-08-09 14:04:23 +0200

    service scroll

---
+++ src/animations/home/scrollService.js
@@ -0,0 +1,36 @@
+import { gsap, ScrollTrigger } from '../../vendor.js';
+
+let context;
+
+function init() {
+  const section = document.querySelector('[data-animate-service=section]');
+  if (section) {
+    const items = section.querySelectorAll('[data-animate-service=item]');
+    context = gsap.context(() => {
+      const tl = gsap.timeline();
+
+      ScrollTrigger.create({
+        trigger: section,
+        animation: tl,
+        start: 'top bottom',
+        end: 'bottom bottom',
+        scrub: 1,
+      });
+
+      tl.from(items, {
+        xPercent: 50,
+        duration: 1,
+        stagger: 0.2,
+      });
+    });
+  }
+}
+
+function cleanup() {
+  context && context.revert();
+}
+
+export default {
+  init,
+  cleanup,
+};

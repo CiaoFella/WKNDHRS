69cd8db604dd73bddc9666fd0f2f25d839404e54
net11FellaJ <julian.fella@neteleven.de>
2024-08-13 22:25:03 +0200

    add normalize logo function

--- src/utilities/helper.js
+++ src/utilities/helper.js
@@ -1,3 +1,5 @@
+import { gsap } from '../vendor.js';
+
 export function unwrapSpanAndPreserveClasses(element) {
   // Select all span elements inside the given element
   const spans = element.querySelectorAll('span');
@@ -41,4 +43,13 @@ export function unwrapSpanAndPreserveClasses(element) {
     // Replace the original span with the new fragment
     span.replaceWith(fragment);
   });
+}
+
+export function normalizeLogo(namespace) {
+  const logo = document.querySelector('[data-logo=wrap]');
+  if (namespace === 'home') {
+    gsap.to(logo, { fontSize: '9vw', duration: 1, ease: 'expo.out' });
+  } else {
+    gsap.to(logo, { fontSize: '2rem', duration: 1, ease: 'expo.out' });
+  }
 }

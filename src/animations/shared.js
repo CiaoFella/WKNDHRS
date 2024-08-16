448b0e926e217a82af06e8c39c4a2c26e264d7b9
net11FellaJ <julian.fella@neteleven.de>
2024-08-16 18:02:25 +0200

    add footer

--- src/animations/shared.js
+++ src/animations/shared.js
@@ -1,3 +1,4 @@
+import footer from './general/footer.js'
 import hero from './shared/hero.js'
 import scrollParallax from './shared/scrollParallax.js'
 import scrollText from './shared/scrollText.js'
@@ -6,12 +7,14 @@ function init() {
   hero.init()
   scrollParallax.init()
   scrollText.init()
+  footer.init()
 }
 
 function cleanup() {
   hero.cleanup()
   scrollParallax.cleanup()
   scrollText.cleanup()
+  footer.cleanup()
 }
 
 export default {

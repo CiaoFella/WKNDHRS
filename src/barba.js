5f49ba27f9c783ef164a079a298c0f92c83c87f8
net11FellaJ <julian.fella@neteleven.de>
2024-08-25 22:44:12 +0200

    adjust page transition

--- src/barba.js
+++ src/barba.js
@@ -1,49 +1,69 @@
 import { closeMenu } from './utilities/helper.js'
 import { proxy } from './utilities/pageReadyListener.js'
-import { fullClipPath, topClipPath } from './utilities/variables.js'
+import { bottomClipPath, fullClipPath, topClipPath } from './utilities/variables.js'
 import { gsap, barba, ScrollTrigger } from './vendor.js'
 
 gsap.registerPlugin(ScrollTrigger)
+
+barba.hooks.before(data => {
+  data.next.container.classList.add('is-animating')
+})
+
+barba.hooks.after(data => {
+  data.next.container.classList.remove('is-animating')
+})
 
 barba.init({
   preventRunning: false,
   transitions: [
     {
       name: 'default-transition',
+      sync: true,
       leave(data) {
-        let done = this.async()
+        const tl = gsap.timeline({ defaults: { duration: 0.75, ease: 'power2.out' } })
+        const done = this.async()
+        const transitionBg = document.querySelector('[data-animate=transition-bg]')
         proxy.pageReady = false
         closeMenu()
-        gsap.to(data.current.container, {
-          opacity: 0.2,
-          duration: 0.3,
-          yPercent: 0.5,
-          transformOrigin: '50% 50%',
-          ease: 'none',
-          force3D: true,
-          onComplete: done,
-        })
+        gsap.set(transitionBg, { display: 'block' })
+        tl.to(
+          data.current.container,
+          {
+            scale: 1.05,
+            opacity: 0.7,
+            transformOrigin: '50% 0%',
+            force3D: true,
+            duration: 1,
+            filter: 'grayscale(100%)',
+            ease: 'power1.in',
+          },
+          0
+        )
+          .fromTo(transitionBg, { scaleY: 0 }, { scaleY: 1, transformOrigin: '50% 0%' }, '<+0.25')
+          .call(done, [], '>')
       },
-      enter(data) {
-        const enterTl = gsap.timeline()
+      after(data) {
+        const transitionBg = document.querySelector('[data-animate=transition-bg]')
+        const currentViewportHeight = window.innerHeight
+        console.log(currentViewportHeight)
+        const enterTl = gsap.timeline({ defaults: { duration: 0.75, ease: 'power2.out' } })
+        gsap.set(transitionBg, { display: 'block' })
         enterTl
+          .to(transitionBg, { scaleY: 0, transformOrigin: '50% 100%' }, 0)
           .fromTo(
             data.next.container,
             {
-              clipPath: topClipPath,
-              height: 0,
+              clipPath: 'polygon(0% 0%, 100% 0%, 100% 0px, 0% 0px)',
             },
             {
-              clipPath: fullClipPath,
-              height: '100vh',
-              duration: 1,
-              delay: 0.1,
-              ease: 'expo.out',
+              clipPath: `polygon(0% 0%, 100% 0%, 100% ${currentViewportHeight}px, 0% ${currentViewportHeight}px)`,
               onComplete: () => {
                 gsap.set(data.next.container, { height: 'auto' })
               },
-            }
+            },
+            '<'
           )
+
           .call(() => (proxy.pageReady = true), [], '<+50%')
       },
     },

40c8a0c3ef29cdfeb2a024a01da4665d1e79932b
net11FellaJ <julian.fella@neteleven.de>
2024-08-26 16:44:42 +0200

    adjust timing

--- src/animations/about/scrollApproach.js
+++ src/animations/about/scrollApproach.js
@@ -73,7 +73,7 @@ function init() {
             },
             '<+0.2'
           )
-          .to(list, { opacity: 1, duration: 0.25, ease: 'power2.inOut' }, '<+0.1')
+          .to(list, { opacity: 1, duration: 0.25, ease: 'power2.inOut' }, '<+0.2')
           .to(
             numbers[index],
             {
